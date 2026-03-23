5991e8f50aaaeac5509f60320569a8247bf8e01c
TAR2003 <tawkir2003@gmail.com>
2026-03-23 22:54:41 +0600

    removed unused files

--- src/alg_approx.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "graph.h"
-#include <vector>
-
-// 2-approximation algorithm (cycle-based): removes two vertices per found cycle
-std::vector<int> two_approximation(const Graph &G);
-
-// Greedy max-degree heuristic
-std::vector<int> greedy_max_degree(const Graph &G);
